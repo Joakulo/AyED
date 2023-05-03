bc1ef65ff543236d8085736cf732862b1575f8f7
Joaquin Emanuel Santa Cruz <79953847+Joakulo@users.noreply.github.com>
2023-05-03 15:30:43 -0300

    Add files via upload

---
+++ Practica/Practica3/tp02/ejercicio2/PilaGenerica.java
@@ -0,0 +1,23 @@
+package tp02.ejercicio2;
+
+public class PilaGenerica<T> {
+	private ListaGenerica<T> List = new ListaEnlazadaGenerica<T>();
+	
+	public void apilar(T x) {
+		List.agregarFinal(x);
+	}
+	
+	public T desapilar() {
+		T x = this.tope();
+		List.eliminarEn(List.tamanio());
+		return x;
+	}
+	
+	public T tope() {
+		return List.elemento(List.tamanio());
+	}
+	
+	public boolean esVacia() {
+		return List.esVacia();
+	}
+}
